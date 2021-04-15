6790abefd1f4b1b208c106bc7a7046dc874a152b
pascal kasi chiza <pascalkasichiza@gmail.com>
2021-04-15 16:51:49 -0700

    CREATE LOGIN

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 
@@ -16,18 +17,66 @@ class AuthController extends Controller
             'email'=>'required|string |unique:users,email', 
             'password'=>'required|string|confirmed',
         );
-        $user=User::create([
-            'name'=>$request->name,
-            'email'=>$request->email, 
-            'password'=>bcrypt($request->password) ,
-        ]);
-        $token=$user->createToken($request->password)->plainTextToken;
+        $validator = Validator::make($request->all(), $validatedData);
+        if ($validator->fails()) {
+            return response()->json([
+                'Message'=>'Parametre manquant',
+                'Error'=>$validator->errors()
+            ], 401);
+        } else {
+            $user=User::create([
+                'name'=>$request->name,
+                'email'=>$request->email, 
+                'password'=>bcrypt($request->password) ,
+            ]);
+            $token=$user->createToken($request->password)->plainTextToken;
+    
+            $response=[ 
+                'user'=>$user,
+                'token'=>$token
+            ];
+    
+            return response($response,201);
+        }
+       
+    }
 
-        $response=[
-            'user'=>$user,
-            'token'=>$token
-        ];
-        
-        return response($response,201);
+
+    public function Login(Request $request)
+    {
+        $validatedData = array(
+            'email'=>'required', 
+            'password'=>'required',
+        );
+        $validator = Validator::make($request->all(), $validatedData);
+        if ($validator->fails()) {
+            return response()->json([
+                'Message'=>'Parametre manquant',
+                'Error'=>$validator->errors()
+            ], 401);
+        } else {
+
+            //Check email 
+            $user=User::where('email',$request->email)->first();
+
+            //Check password
+            if(!$user || !Hash::check($request->password,$user->passwrod)){
+                return response([
+                    'Message'=>['Uttilisateur not trouver'],
+                    'USER ERROR'=>$user,
+                ],404);
+            }
+            
+            
+            $token=$user->createToken($request->password)->plainTextToken;
+    
+            $response=[ 
+                'user'=>$user,
+                'token'=>$token
+            ];
+    
+            return response($response,201);
+        }
+       
     }
 }
