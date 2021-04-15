fb4a348ea2ea90f18be07ed4802fc508695429b6
pascal kasi chiza <pascalkasichiza@gmail.com>
2021-04-15 13:37:42 -0700

    create migration

--- database/migrations/2021_04_15_195752_create_produts_table.php
+++ database/migrations/2021_04_15_195752_create_produts_table.php
@@ -16,6 +16,7 @@ class CreateProdutsTable extends Migration
         Schema::create('produts', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('slug');
             $table->string('description')->nullable();
             $table->decimal('price',5,2); 
             $table->timestamps();
