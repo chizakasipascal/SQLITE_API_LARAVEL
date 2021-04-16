ef7336002c6f0146b62af9434303680c7bac4e94
pascal kasi chiza <pascalkasichiza@gmail.com>
2021-04-15 20:45:13 -0700

    Reagistment de code de validation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,71 +13,68 @@ class AuthController extends Controller
     public function Register(Request $request)
     {
         $validatedData = array(
-            'name'=>'required|string',
-            'email'=>'required|string |unique:users,email|email', 
-            'password'=>'required|string|confirmed',
+            'name' => 'required|string',
+            'email' => 'required|string |unique:users,email|email',
+            'password' => 'required|string|confirmed',
         );
         $validator = Validator::make($request->all(), $validatedData);
         if ($validator->fails()) {
             return response()->json([
-                'Message'=>'Parametre manquant',
-                'Error'=>$validator->errors()
+                'Message' => 'Parametre manquant',
+                'Error' => $validator->errors()
             ], 401);
         } else {
-            $user=User::create([
-                'name'=>$request->name,
-                'email'=>$request->email, 
-                'password'=>bcrypt($request->password) ,
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => bcrypt($request->password),
             ]);
-            $token=$user->createToken($request->password)->plainTextToken;
-    
-            $response=[ 
-                'user'=>$user,
-                'token'=>$token
+            $token = $user->createToken($request->password)->plainTextToken;
+
+            $response = [
+                'user' => $user,
+                'token' => $token
             ];
-    
-            return response($response,201);
+
+            return response($response, 201);
         }
-       
     }
 
     /**
-     * Method 
+     * Method help you to login
      */
     public function Login(Request $request)
     {
-        // $validatedData = array(
-        //     'email'=>'required', 
-        //     'password'=>'required',
-        // );
-        // $validator = Validator::make($request->all(), $validatedData);
-        // if ($validator->fails()) {
-        //     return response()->json([
-        //         'Message'=>'Parametre manquant',
-        //         'Error'=>$validator->errors()
-        //     ], 401);
-        // }  
+        $validatedData = array(
+            'email' => 'required',
+            'password' => 'required',
+        );
+        $validator = Validator::make($request->all(), $validatedData);
+        if ($validator->fails()) {
+            return response()->json([
+                'Message' => 'Parametre manquant',
+                'Error' => $validator->errors()
+            ], 401);
+        }
 
-            //Check email 
-            $user=User::where('email',$request->email)->first();
-            //Check password
-            if (!$user ||!Hash::check($request->password,$user->password)) {
-                return response([
-                    'Message'=>['Coodonner not valider']
-                    //'USER ERROR'=>$user,
-                ],401);
-            }
+        //Check email 
+        $user = User::where('email', $request->email)->first();
+        //Check password
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response([
+                'Message' => ['Coodonner not valider']
+                //'USER ERROR'=>$user,
+            ], 401);
+        }
 
-            $token=$user->createToken($request->password)->plainTextToken;
-    
-            $response=[ 
-                'user'=>$user,
-                'token'=>$token
-            ];
-    
-            return response($response,201);
-        
-       
+        $token = $user->createToken($request->password)->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
     }
 
     /***
@@ -85,10 +82,11 @@ class AuthController extends Controller
      * 
      * Helo you to logout ! 
      */
-    public function Logout(Request $request){
+    public function Logout(Request $request)
+    {
         auth()->user()->tokens()->delete();
         return [
-            'Message'=>'Logout !!'
+            'Message' => 'Logout !!'
         ];
     }
 }
